258b623dae601d2ef69064c2997e8404aca20b08
Sören Michaels <soeren.michaels@gmail.com>
2021-03-19 12:04:04 +0100

    added tests

--- pyfinra/tests/test_ticker.py
+++ pyfinra/tests/test_ticker.py
@@ -1,5 +1,8 @@
 from pyfinra import Ticker
 
+
 def test_Ticker():
-    Ticker("Gme").quote() 
-    
+    gme = Ticker("Gme") 
+    gme.quote()
+    gme.financials_balancesheet()
+    gme.financials_inc_statement()
